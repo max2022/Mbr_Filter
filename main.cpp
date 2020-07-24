335fd7b20601a80b1902afb5a7c7c801981a5fb7
buddhi1 <buddhiashan8@gmail.com>
2020-07-23 20:50:00 -0500

    comments added. getCMBRList() applied an upper bound.

--- main.cpp
+++ main.cpp
@@ -26,8 +26,9 @@ typedef boost::geometry::model::polygon<point_xy > polygon;
 polygon getMBR(float px, float py) {
     using namespace boost::assign;    
 
+    // return MBR
     polygon ret;
-    // threshhold constant
+    // threshhold constant detrmines the MBR size for an instance
     float const d = 2.0;
 
     // Create points to represent a rectagle.
@@ -46,23 +47,28 @@ polygon getMBR(float px, float py) {
 }
 
 // returns MBR for a selected feature
-polygon** getMBRList(struct table_row *data, int li) {
-    int ptr[FEATURES] = {0};
+polygon** getMBRList(struct table_row *data, int li, int *ptr) {
+    // output 2D polygon. 
     polygon** arr = 0;
 
+    // Creating 1st-D
     arr = new polygon *[FEATURES];
 
     for (int i = 0; i < li; ++i)
-    {  
+    { 
+         // only creates the 2nd dimension for the selected indices,
+        // iff the  (feature id -1) equals to 1st-D index
+        // helps to not assign 2nd-D to the unwanted indices
         if (ptr[data[i].id-1] == 0)
          {
             arr[data[i].id-1] = new polygon [INST];
          }
          // if (data[i].id-1 == 8)
          //  {
-            // std::cout << data[i].id << ": " << data[i].x << ": " <<data[i].y << ": " << ptr[data[i].id-1] <<std::endl;              
+         //    std::cout << data[i].id << ": " << data[i].x << ": " <<data[i].y << ": " << ptr[data[i].id-1] <<std::endl;              
          //  } 
 
+        // calculate MBR using the getMBR() and assign it to the relavant feature instance
         arr[data[i].id-1][ptr[data[i].id-1]++] = getMBR(data[i].x, data[i].y);
     }
 
@@ -72,41 +78,76 @@ polygon** getMBRList(struct table_row *data, int li) {
 
 // returns CMBR for a given two MBRs
 polygon getCMBR(polygon a, polygon b) {
+    // Dequeue to save all the intersections from the input polygons
+    // We may only have rectangles as input and output always wil be a one rectangle
     std::deque<polygon> output;
+    // output rectangle
     polygon ret;
+
+    // check intersection between a and b
     boost::geometry::intersection(a, b, output);
-    ret = output.front();
+    // if an intersection exists, it will be assigned to the output. 
+    // At max, we may have only one intersection for two given rectangles.
+    if (!output.empty())
+    {
+        ret = output.front();        
+    }
 
     return ret;
 }
 
-polygon** getCMBRList(polygon **mbrs, int a, int b) 
+polygon** getCMBRList(polygon **mbrs, int a, int b, int *ptr) 
 {
     // 2D array to hold all CMBRS. Should represents levels
     polygon** arr = 0;
 
-    int level = 0, insid = 0, len = 10000;
-    int ss = 100000;  
+    // level controls the layer
+    int level = 0;
+    // controls next available sapce in the layer array
+    int insid = 0;
+    // memory safe variable
+    // controls max number of CMBRs for each layer
+    int ss = 100;  
+
+    // Create 1st-D for the output. Assumed we have only 12 features
     arr = new polygon *[12]; 
+
+    // allocate 2nd-D for the layer 1
     arr[level] = new polygon [ss]; 
 
-    //nested loop to check all the combinations of instances and then check CMBRs
-    for (int i = 0; i < len; ++i)
+    // temporary varibale to track of calculated CMBR
+    polygon cmbr;
+
+    //nested loop to check all the CMBRs for all combinations of instances 
+    for (int i = 0; i < ptr[a]; ++i)
     { 
-        for (int j = 0; j < len; ++j)
+        for (int j = 0; j < ptr[b]; ++j)
         {
-            // if (mbrs[a][i] != 0 && mbrs[b][j] != 0)
-            // {
-                arr[level][insid++] = getCMBR(mbrs[a][i], mbrs[b][j]);   
-                std::cout << i << ": " << j << std::endl;             
-            // }   
-            if (ss <= insid)
+            cmbr= getCMBR(mbrs[a][i], mbrs[b][j]);
+            // std::cout << !boost::geometry::is_empty(cmbr) <<std::endl;              
+            // checking if the CMBR exists
+            if (!boost::geometry::is_empty(cmbr))    
             {
+                arr[level][insid++] = getCMBR(mbrs[a][i], mbrs[b][j]);   
+            } 
+            // memory safe condition
+            // if we reach max memory for the layer combination allocation, 
+            // it may stop assigning further CMBRS
+            if (ss <= insid)
+            {           
                 break;
             }       
         }
+        // prints the outer feature instance id-1
+        std::cout << i  << std::endl;
+
+        // memory safe condition
+        // if we reach max memory for the layer combination allocation, 
+        // it may stop assigning further CMBRS
         if (ss <= insid)
         {
+            std::cout << "Error: CMBR array not enough!!!" <<std::endl;              
+
             break;
         }
     }
@@ -117,26 +158,36 @@ polygon** getCMBRList(polygon **mbrs, int a, int b)
 
 int main()
 {
-    // read data into a 2D array
+    //array to hold the number of instances for each feature
+    static int feature_sizes[FEATURES] = {0};
+
+    // read data into a table_row structure type 1D array
     struct table_row *dat;
     dat = createArray("Seattle2012.csv");
-    // std::cout << dat[0].x << std::endl;
 
-    // calculate MBR for all the datapoints
-    polygon**  mbr_array = getMBRList(dat, ROWS);
-    // getMBRList(dat, ROWS);
-    std::cout <<  boost::geometry::wkt(mbr_array[41][0]) << std::endl;
+    // calculate MBR for all the datapoints. 
+    // returns a 2D array. 1st-D : Features, 2nd-D: instances per each feature 
+    polygon**  mbr_array = getMBRList(dat, ROWS, feature_sizes);
 
+    // prints the feature sizes 
+    // for (int i = 0; i < FEATURES; ++i)
+    // {
+    //     std::cout << feature_sizes[i] << std::endl;
+    // }
 
-    // calculate CMBR recursively for each of the features
-    polygon x = getMBR(5,5);
-    polygon y = getMBR(2,2);
-    polygon cmbr;
-    cmbr = getCMBR(x, y); 
-    polygon**  cmbr_array = getCMBRList(mbr_array, 0, 7);
-
-
+    // testing getMBR()
+    // polygon x = getMBR(5,5);
+    // polygon y = getMBR(2,2);
+    // polygon cmbr;
+    // cmbr = getCMBR(x, y);
     // std::cout << boost::geometry::wkt(cmbr) << std::endl;
+
+
+    // calculate CMBR for 2 selected features. Works as the layer 1 output
+    polygon**  cmbr_array = getCMBRList(mbr_array, 0, 7, feature_sizes);
+
+     
+
     // std::cout <<  << std::endl;
 
     // print all CMBRs
