f8281df544d1b83b5f52b61b9b2064be03070e31
zdx978 <zdx978@login01.cm.cluster>
2020-08-22 15:00:05 -0500

    removed boost and getting lots of instances upto 10th layer

--- batch.sh
+++ batch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -p testing
-#SBATCH -t 4:00:00
+#SBATCH -p defq
+#SBATCH -t 10:00:00
 #SBATCH --ntasks-per-node 1
  
-g++ main_filter.cpp -o a.out -std=gnu++11
-./a.out
+g++ main-v2.1.cpp -o b.out -std=gnu++11
+./b.out
 
  
 
